01436e3f614f5bbee1bbc3da49e8879822726c4f
Diinolas <42970774+Diinolas@users.noreply.github.com>
2021-10-17 19:09:34 +1100

    helpme

--- reader/assets/js/main.js
+++ reader/assets/js/main.js
@@ -95,4 +95,11 @@ $('.famouslogobutton').click(function() {
 });
 
 
-/* ---- */
+/* Background Animation */
+
+const el = document.querySelector("#body");
+
+el.addEventListener("mousemove", (e) => {
+  el.style.backgroundPositionX = -e.offsetX + "px";
+  el.style.backgroundPositionY = -e.offsetY + "px";
+});
